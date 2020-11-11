60cbe39047e5e91bb89314f885503d9dbe7fe447
rahamanar <rahamanar123@gmail.com>
2020-11-11 21:28:22 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/RAZORPAY_APP/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.RAZORPAY_APP
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/home.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'package:toast/toast.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Razorpay razorpay;
+  TextEditingController textEditingController = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    razorpay = new Razorpay();
+
+    razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
+    razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerErrorFailure);
+    razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    razorpay.clear();
+  }
+
+  void openCheckout() {
+    var options = {
+      "key": "rzp_test_kbBm1UhdSFY2YO",
+      "amount": num.parse(textEditingController.text) * 100,
+      "name": "Sample App",
+      "description": "Payment for some random product",
+      "prefill": {"contact": "2323232323", "email": "vita@gmail.com"},
+      "external": {
+        "wallets": ["paytm"]
+      }
+    };
+
+    try {
+      razorpay.open(options);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  void handlerPaymentSuccess() {
+    print("Pament success");
+    Toast.show("Pament success", context);
+  }
+
+  void handlerErrorFailure() {
+    print("Pament error");
+    Toast.show("Pament error", context);
+  }
+
+  void handlerExternalWallet() {
+    print("External Wallet");
+    Toast.show("External Wallet", context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Razor Pay"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: textEditingController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(hintText: "Enter Amount"),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            RaisedButton(
+              color: Colors.blue,
+              child: Text(
+                "Pay",
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                openCheckout();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
